2a90795b377f67a6c5ba8465dededd164a974c0d
Prateek <Prateekrajgrd74@gmail.com>
2024-02-06 13:22:11 +0530

    syn_asyn.java

--- syn_asyn.js
+++ syn_asyn.js
@@ -48,6 +48,7 @@ fs.stat('input.txt', 'r+', function(err,stats){
 });
 */
 
+/* write into file
 
 var fs =require("fs");
 console.log("Going to write into existing file");
@@ -66,4 +67,28 @@ fs.writeFile('input.txt', 'Hello World',function(err){
         console.log("Asynchronus read: "+data.toString());
     })
 });
+*/
 
+var fs= require("fs");
+var buff = new Buffer(1024);
+
+console.log("Going to open an existing file");
+fs.open('input.txt','r+',function(err,fd){
+    if(err){
+        return console.log(err);
+    }
+    console.log("File opened successfully");
+    console.log("Going to read the file");
+
+    fs.read(fd,buff,0,buff.length,0,function(err,bytes){
+        if(err){
+            console.log(err)
+        }
+        console.log(bytes+ " bytes read");
+
+        //Print only read bytes to avoid junk
+        if((bytes>0)){
+            console.log(buff.slice(0,bytes).toString());
+        }
+    });
+});
